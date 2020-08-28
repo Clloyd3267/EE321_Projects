6f1678618395423183c3175a6865b7a6ebf4b3fa
Clloyd3267 <37284048+Clloyd3267@users.noreply.github.com>
2020-08-28 11:19:31 -0400

    Added Comment framework and class/function headers

---
+++ Project 1/src/Main/main.py
@@ -0,0 +1,24 @@
+################################################################################
+# Name        : main.py
+# Author(s)   : Kyle Bielby, Chris Lloyd, Stephen Epp, Philip Hubbe
+# Class       : EE321 (Project 1)
+# Due Date    : 2020-09-18
+# Description : CDL=> Here Later
+################################################################################
+
+# Imports
+
+def main():
+    """
+    Main function to demo audio effects including:
+    - Delay
+    - Echo
+    - Reverb
+    
+    Parameters: (None)
+    Returns:    (None)
+    """ 	
+    print("hello world!")
+
+if __name__ == "__main__":
+    main()
---
+++ Project 1/src/Part 2 - Echo/echo.py
@@ -0,0 +1,47 @@
+################################################################################
+# Name        : echo.py
+# Author(s)   : Kyle Bielby, Chris Lloyd, Stephen Epp, Philip Hubbe
+# Class       : EE321 (Project 1)
+# Due Date    : 2020-09-18
+# Description : CDL=> Here Later
+################################################################################
+
+# Imports
+
+class AudioEcho:
+    """
+        A class to add echo to an audio file.
+
+        Attributes: (None)
+        """
+
+    def __init__(self):
+        """
+        The constructor for class AudioEcho.
+        """
+
+    ############################################################################
+    # Main Funcs
+    ############################################################################
+    def importAudio(self, audioFileName = "Hey.mp3"):
+        """
+        Function to add import an mp3 file to a CDL=> Add type later.
+
+        Parameters:
+            audioFileName (string): The input audio filename. 
+                                    (Defaults to "Hey.mp3")
+
+        Returns:
+            (0): No errors, (Anything else): Errors.
+        """
+            
+    def echo(self, audioData):
+        """
+        Function to add echo to audio.
+
+        Parameters:
+            audioData (CDL=> Here): The input audio.
+
+        Returns:
+            (0): No errors, (Anything else): Errors.
+        """
