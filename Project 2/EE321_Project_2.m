440037d4143e28fab6a5cbd75d1877402f5a003d
Clloyd3267 <37284048+Clloyd3267@users.noreply.github.com>
2020-09-24 18:27:55 -0400

    Revert "Cleaned up sections and got them all to the same format/style"
    This reverts commit 929aca73e65c2424550772bb0e1914f504ed4d36.

--- Project 2/EE321_Project_2.m
+++ Project 2/EE321_Project_2.m
@@ -1,10 +1,10 @@
-%%------------------------------------------------------------------------------
+% -------------------------------------------------------------------------
 % Name        : EE321_Project_2.m
 % Author(s)   : Kyle Bielby, Chris Lloyd, Stephen Epp, Philip Hubbe
 % Class       : EE321 (Project 2)
 % Due Date    : 2020-10-02
-% Description : Data analysis using Dimensionality Reduction and PCA techniques
-% ------------------------------------------------------------------------------
+% Description : CDL=> Fill in later
+% -------------------------------------------------------------------------
 
 %% Original File ("gen_data.csv"):
 
@@ -19,12 +19,8 @@ data = readmatrix("gen_data.csv");
 % Output dimensions of data
 [r, c] = size(data)
 
-% Data dimensions: 200 rows, 3 cols
-
 % Display data in a scatterplot
 scatter3(data(:,1),data(:,2),data(:,3));
-
-% Two clusters of data points on top of each other.
 
 % Principal Component Analysis (PCA):
 
@@ -49,9 +45,9 @@ eigValSorted = eigValue(ind);
 eigVecSorted = Qd(:,ind);
 
 % Condition Number
-cNumber = max(eigValue) / min(eigValue);
+Cd = max(eigValue) / min(eigValue);
 
-% cNumber >> 1 indicating Highly ill-conditioned matrix
+% Cd >> 1 indicating Highly ill-conditioned matrix
 
 % Calculate energy for each Eigenvalue
 % eigenEnergy(n) = eigValue(n)^2 / (ΣeigValue()^2)
@@ -81,100 +77,76 @@ scatter(reconstructedData,zeros(size(reconstructedData)));
 % the projection of the three dimensional data clusters onto a singular 
 % dimension.
 
-%% Higher Dimensional Data ("gen_data2.csv"):
-
-% Clear the workspace and console
+%% 3. Higher Dimensional Data
 clear; clc;
+%load data
+newData = readmatrix("gen_data2.csv");
 
-% Examine the Data:
+% Find Dimensionality of the data
+[row, c] = size(newData);
 
-% Read in data
-data = readmatrix("gen_data2.csv");
+% Visualize the data
 
-% Output dimensions of data
-[r, c] = size(data)
-
-% Data dimensions: 300 rows, 3 cols
-
-% Display data in a scatterplot
-figure
-scatter3(data(:,1),data(:,2),data(:,3));
-
-% Three clusters of data points with two next to each other both on top of
-% the third.
-
-% Principal Component Analysis (PCA):
-
-% Calculate Mean
-m = mean(data);
+% find the mean of the data
+gen2Mean = mean(newData);
 
 % Subtract mean from each data sample
-dm = data - repmat(m,r,1);
+dm = newData - repmat(gen2Mean, row, 1);
 
 % Find Co-Variance Matrix
-cd=(1/(r-1))*(dm.'*dm);
+cd=(1/(row-1))*(dm.'*dm);
 
 % Do EigenDecomposition
 [Qd, Dd] = eig(cd);
 
-% Dimensionality Reduction:
-
-% Sort the eigenvalues in decending order
+% Sort the eigenvalues in descending order
 eigValue = diag(Dd);
 [~,ind] = sort(eigValue, "descend");
 eigValSorted = eigValue(ind);
 eigVecSorted = Qd(:,ind);
 
-% Condition Number
-cNumber = max(eigValue) / min(eigValue);
+%find sum of squares for the eigenvectors
+sumSquares = 0;
 
-% cNumber >> 1 indicating Highly ill-conditioned matrix
-
-% Calculate energy for each Eigenvalue
-% eigenEnergy(n) = eigValue(n)^2 / (ΣeigValue()^2)
-eigenEnergy = eigValSorted.^2./sum(eigValSorted.^2);
-
-% Find number of Eigen values needed to represent 90% of data
-eigenEnergySum = 0;
-for i = 1:length(eigenEnergy)
-  eigenEnergySum = eigenEnergySum + eigenEnergy(i);
-  if eigenEnergySum >= 0.9
-    num_comp = i;
-    break
-  end
+%sum the squared eigen values
+for eigenVectors = 1:size(eigValue)
+    sumSquares = sumSquares + eigValue(eigenVectors, 1) * eigValue(eigenVectors, 1);
 end
 
-% In this case, 2 Eigen values is enough
+%display percentage energy contained in eigen vector
+percentageEnergy = size(Dd):1;
+for eigenVector = 1:size(Dd)
+    percentageEnergy(eigenVector, 1) = Dd(eigenVector, eigenVector) / sumSquares; %display percentages as assigning
+end
 
-% Select the eigenvector(s) corresponding to the highest valued eigenvalue
-prinComps = eigVecSorted(:,1:num_comp);
-reconstructedData = data * prinComps;
+%Apply PCA and find the minimum number of dimensions needed to
+%represent > 90% of the energy of the data
+Dimensions = 0;
+sum = 0;
+for i = size(percentageEnergy):-1:1
+    sum = sum + percentageEnergy(i, 1);
+    if sum < .9
+        Dimensions = Dimensions + 1;
+    end
+end
 
-% Visualize data
+%Reconstruct the low dimensional data and visualize it.
+prin_comps = eigVecSorted(:,1:Dimensions);
+reconstructedData = newData * prin_comps;
+
+%display figure of reconstructed data
 figure
-scatter(reconstructedData(:,1),reconstructedData(:,2));
 
-% The plot indicates that there are three clusters of data that resulted from
-% the projection of the three dimensional data clusters onto two dimensions.
+scatter3(reconstructedData(:,1), reconstructedData(:,2), reconstructedData(:,3));
+title('gen data2 Dimensioned plot');
 
-%% Keystroke Data ("keystroke_data.csv"):
-
-% Clear the workspace and console
-clear; clc;
-
-% Examine the Data:
-
-% Read in data
+%% 4. Keystroke Data
+%clear; clc;
 data = readmatrix("keystroke_data.csv");
 
 % Output dimensions of data
-[r, c] = size(data)
+[r, c] = size(data);
 
-% Data dimensions: 200 rows, 21 cols
-
-% Principal Component Analysis (PCA):
-
-% Calculate Mean
 m = mean(data);
 
 % Subtract mean from each data sample
@@ -186,43 +158,19 @@ cd=(1/(r-1))*(dm.'*dm);
 % Do EigenDecomposition
 [Qd, Dd] = eig(cd);
 
-% Dimensionality Reduction:
-
-% Sort the eigenvalues in decending order
 eigValue = diag(Dd);
-[~,ind] = sort(eigValue, "descend");
+[~,ind] = sort(eigValue, 'descend');
 eigValSorted = eigValue(ind);
 eigVecSorted = Qd(:,ind);
 
-% Condition Number
-cNumber = max(eigValue) / min(eigValue);
+% Condition Number - Cd >> 1 indicating Highly ill-conditioned matrix
+Cd = max(eigValue) / min(eigValue);
 
-% cNumber >> 1 indicating Highly ill-conditioned matrix
+[eig_value_nrg, num_comp] = eigValueNRG(eigValue);
 
-% Calculate energy for each Eigenvalue
-% eigenEnergy(n) = eigValue(n)^2 / (ΣeigValue()^2)
-eigenEnergy = eigValSorted.^2./sum(eigValSorted.^2);
-
-% Find number of Eigen values needed to represent 90% of data
-eigenEnergySum = 0;
-for i = 1:length(eigenEnergy)
-  eigenEnergySum = eigenEnergySum + eigenEnergy(i);
-  if eigenEnergySum >= 0.9
-    num_comp = i;
-    break
-  end
-end
-
-% In this case, 1 Eigen value is enough
-
-% Select the eigenvector(s) corresponding to the highest valued eigenvalue
-prinComps = eigVecSorted(:,1:num_comp);
-reconstructedData = data * prinComps;
+prin_comps = eigVecSorted(:,1:num_comp);
+reconstructed_data = data*prin_comps;
 
 % Visualize data
 figure
-scatter(reconstructedData,zeros(size(reconstructedData)));
-
-% The plot indicates that there are two clusters of data that resulted from
-% the projection of the 21 dimensional data clusters onto a singular 
-% dimension.
+scatter(reconstructed_data,zeros(size(reconstructed_data)));
