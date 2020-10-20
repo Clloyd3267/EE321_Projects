6e05f8b1d230f184766ebe20150a00abdd498dbe
Clloyd3267 <37284048+Clloyd3267@users.noreply.github.com>
2020-10-20 10:11:14 -0400

    Initial Commit for project 3

---
+++ Project 3/EE321_Project_2.m
@@ -0,0 +1,11 @@
+%%------------------------------------------------------------------------------
+% Name        : EE321_Project_3.m
+% Author(s)   : Kyle Bielby, Chris Lloyd, Stephen Epp, Philip Hubbe
+% Class       : EE321 (Project 3)
+% Due Date    : 2020-10-23
+% Description : Calculating Error in Fourier Series Depending on the number of 
+%               terms used.
+% ------------------------------------------------------------------------------
+
+% Clear the workspace and console
+clear; clc;
