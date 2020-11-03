73dc762fcaed80d468b124d7334da3f61de46fe5
Clloyd3267 <37284048+Clloyd3267@users.noreply.github.com>
2020-11-03 16:56:13 -0500

    Redid Part 1 and 2

--- Project 4/EE321_Project_4.m
+++ Project 4/EE321_Project_4.m
@@ -1,153 +1,163 @@
-clear;
-clc;
-N = 1000; % Number of symbols to be transmitted
-A = 1; % We can adjust this as needed. This is directly related to the transmit power
-x = A*(2*(randn(N,1)>0) - 1); % Generates +A and -A; both equally likely
+%%------------------------------------------------------------------------------
+% Name        : EE321_Project_4.m
+% Author(s)   : Kyle Bielby, Chris Lloyd, Stephen Epp, Philip Hubbe
+% Class       : EE321 (Project 4)
+% Due Date    : 2020-11-13
+% Description : Data Classification using Matlab
+% ------------------------------------------------------------------------------
 
-%%Section 3.1
-%scatter plot to visualize transmitted symbols
-scatter(zeros(size(x)), x);
-% scatter(x(:,1),x(:,2));   %KAB => NOT sure if this is the kind of scatterplot that he wants
-% plotmatrix(x);
-% plot(x);
+%% Part 1 - (Fixed A) Working with received data (TX data + noise) for two data states
 
-%received signal generation
-var_n = 0.01; % Noise variance
+% Clear the workspace and console
+clear; clc;
+
+% Create transmit data
+N = 1000;                      % Number of symbols to be transmitted
+A = 1;                         % Data amplitude (relates to signal power)
+var_n = 0.01;                  % Noise variance
+x = A*(2*(randn(N,1)>0) - 1);  % Generates +A and -A randomly but equally likely
+
+% Plot data on a scatter plot to visualize transmitted symbols
+figure
+scatter(x, zeros(size(x)))
+title("Transmit Signal For Part 1")
+xlabel("Amplitude (-A to +A)")
+
+% Generate received signal as transmit signal + noise
 n = randn(N,1)*sqrt(var_n); % Zero-mean Gaussian noise with variance var_n
-r = x + n; % Received signal
-scatter(r,zeros(size(r)));   %KAB => not sure if this is the kind of scatterplot needed but it looks pretty good
+r = x + n;                  % Received signal
+
+% Plot data on a scatter plot to visualize received symbols
+figure
+scatter(r, zeros(size(r)))
+title("Received Signal For Part 1")
+xlabel("Amplitude (-A to +A)")
+
+% % Visualize data with r = 0 CDL=> Needed?
+% r = zeros(size(x));
 % plot(r);
 
-%visualize data with r = 0
-% r = zeros(size(x));
-% % plot(r);
-
-% receive signal value decision algorithm
-for i = 1:1:1000
-    if r(i) > 0
-        received(i) = A;
-    elseif r(i) < 0
-        received(i) = -1 * A;
-    end
-end
-    
-%plot received data
-scatter(received, zeros(size(received)));
-
-%% Section 3.2
-%find the number of incorrect guesses
-incorrectReceive = 0;
-for i = 1:1:1000
-   if x(i) ~= received(i)
-    incorrectReceive = incorrectReceive + 1;
-   end
-end
-
-%compute probability of error
-Pe = incorrectReceive / N;
-
-%change A to 2
-A = 2;
-x = A*(2*(randn(N,1)>0) - 1); % Generates +A and -A; both equally likely
-r = x + n; % Received signal
-scatter(r,zeros(size(r)));   %KAB => not sure if this is the kind of scatterplot needed but it looks pretty good
-
-% receive signal value decision algorithm
-for i = 1:1:1000
-    if r(i) > 0
-        received(i) = A;
-    elseif r(i) < 0
-        received(i) = -1 * A;
+% Decision algorithm for receive signal value
+r_norm = zeros(size(x));
+for i = 1:1:N
+    if r(i) > 0             % If positive
+        r_norm(i) = A;
+    else                    % If negative or 0
+        r_norm(i) = -1 * A;
     end
 end
 
-%find the number of incorrect guesses
+% Plot normalized receive data
+figure
+scatter(r_norm, zeros(size(r_norm)))
+title("Normalized Receive Signal For Part 1")
+xlabel("Amplitude (-A to +A)")
+
+% Error Characterization
+
+% Find the number of incorrect guesses
 incorrectReceive = 0;
-for i = 1:1:1000
-   if x(i) ~= received(i)
-    incorrectReceive = incorrectReceive + 1;
-   end
+for i = 1:1:N
+    if x(i) ~= r_norm(i)
+        incorrectReceive = incorrectReceive + 1;
+    end
 end
 
-%compute probability of error
-%KAB TODO: fix this part of the program
-j = 1; % index
-% for A = (sqrt(10 * var_n)) : 0.01 : (sqrt(10 .^ 4 * var_n)) %vary A so SNR varies from 10dB to 40dB
-    for A = [0.01 1]
-%     A = 0.01;
-    x = A*(2*(randn(N,1)>0) - 1); % Generates +A and -A; both equally likely
-    r = x + n; % Received signal
-    
-    % receive signal value decision algorithm
-    for i = 1:1:1000
-        if r(i) > 0
-            received(i, 1) = A;
-        elseif r(i) < 0
-            received(i, 1) = -1 * A;
+% Compute the probability of error
+Pe = incorrectReceive / N
+
+%% Part 2 - (Varied A) Working with received data (TX data + noise) for two data states
+
+var_n = 0.01;  % Noise variance
+
+for SNR_db = [10:1:40]
+    SNR = 10^(SNR_db/10);   % Calculate Linear SNR (X_db = 10*log(X))
+    A = sqrt(SNR*var_n^2);  % Calculate value of A (SNR = (A^2)/(var_n^2))
+
+    % Create transmit data
+    x = A*(2*(randn(N,1)>0) - 1);  % Generates +A and -A randomly but equally likely
+
+    % Generate received signal as transmit signal + noise
+    n = randn(N,1)*sqrt(var_n); % Zero-mean Gaussian noise with variance var_n
+    r = x + n;                  % Received signal
+
+    % Decision algorithm for receive signal value
+    r_norm = zeros(size(x));
+    for i = 1:1:N
+        if r(i) > 0             % If positive
+            r_norm(i) = A;
+        else                    % If negative or 0
+            r_norm(i) = -1 * A;
         end
     end
-    
-    %find the number of incorrect guesses
-    incorrectReceive = 0;
-    for k = 1:1:1000
-       if x(k) ~= received(k , 1)
-        incorrectReceive = incorrectReceive + 1;
-       end
-    end
-    
-    % store probabiltity of error and SNR values 
-    Pe = incorrectReceive / 1000.000;
-    PeValues(j) = incorrectReceive / 1000.000;
-    SNRValues(j) = 10 * log10(A ^ 2 / var_n);
 
-    j = j + 1;  %increment stored index
+    % Error Characterization
+
+    % Find the number of incorrect guesses
+    incorrectReceive = 0;
+    for i = 1:1:N
+        if x(i) ~= r_norm(i)
+            incorrectReceive = incorrectReceive + 1;
+        end
+    end
+
+    % Compute the probability of error
+    Pe = incorrectReceive / N;
+
+    % Add values to array
+    PeValues(SNR_db - 9) = Pe;
+    SNRValues(SNR_db - 9) = SNR_db;
 end
 
-%plot probability vs SNR
-% semilogy(SNRValues, PeValues);    
+% Plot Probablity of Error vs SNR
+figure
+scatter(SNRValues, PeValues)
+title("Probablity of Error vs SNR")
+xlabel("SNR (db)")
+ylabel("Probablity of Error (Pe)")
 
 %%Section 3.3
 % N = 1000;
 % A = 5;
 % x = A*((2*(randn(N,1)>0)) - 1); % Generates +A and -A; both equally likely
-% y = 2*(randn(N,1)>0) - 1;   % Generates 1 and -1; both equally 
-%                             % this will be used to determine if 
-%                             % if A is placed on the x-axis or the 
+% y = 2*(randn(N,1)>0) - 1;   % Generates 1 and -1; both equally
+%                             % this will be used to determine if
+%                             % if A is placed on the x-axis or the
 %                             % y axis
-%                             
+%
 % for i = 1:1:1000
-%     if y(i) == 1        % if y = 1 place on y-axis 
+%     if y(i) == 1        % if y = 1 place on y-axis
 %         y(i) = x(i);
 %         x(i) = 0;
 %     elseif y(i) == -1   % if y = -1 place on x-axis
 %         y(i) = 0;
 %     end
 % end
-%                                      
+%
 % scatter(x,y);
-% 
+%
 % % test noise simulation
 % var_n = 0.01; % Noise variance
 % n = randn(N,1)*sqrt(var_n); % Zero-mean Gaussian noise with variance var_n
-% m = randn(N,1)*sqrt(var_n); % Zero-mean Gaussian noise with variance var_n 
+% m = randn(N,1)*sqrt(var_n); % Zero-mean Gaussian noise with variance var_n
 % r = x + n; % Received signal
-% y = y + m; % 
-% 
+% y = y + m; %
+%
 % scatter(r,y);
 
 
 % for N = [1000 5000] %select a large enough value of N for a smooth curve
 %     for A = [0.01 1]  %TODO change to neeeded values
 %         x = A*((2*(randn(N,1)>0)) - 1); % Generates +A and -A; both equally likely
-%         y = 2*(randn(N,1)>0) - 1; % Generates 1 and -1; both equally 
-%                                      % this will be used to determine if 
-%                                      % if A is placed on the x-axis or the 
+%         y = 2*(randn(N,1)>0) - 1; % Generates 1 and -1; both equally
+%                                      % this will be used to determine if
+%                                      % if A is placed on the x-axis or the
 %                                      % y axis
-% 
-%         
+%
+%
 %     end
-%     
-%     
+%
+%
 % end
 
 
